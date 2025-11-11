6c1c3e48193095027a8e4c5b29a0018c02512456
iMved2075 <ved20750327@gmail.com>
2025-11-11 19:50:01 +0530

    Add user authentication methods and update environment variables for JWT

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        trim: true
+        trim: true,
+        index: true
     },
     profilePicture: {
         type: String,
@@ -39,5 +43,43 @@ const userSchema = new mongoose.Schema({
         trim: true
     }
 },{timestamps: true});
+
+
+userSchema.pre('save', async function (next) {
+    if( !this.isModified('password')) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model('User', userSchema);
